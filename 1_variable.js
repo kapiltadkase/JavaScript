08a6c2ddb49b35a2aefd0f5cae264d2def71f96b
kapiltadkase <kapiltadkase770@gmail.com>
2026-04-04 23:51:34 +0530

    datatypes

---
+++ 1_variable.js
@@ -0,0 +1,12 @@
+const accountId =  1234556;     // constant cant be reassigned
+  
+let user = "Kapil";               // can be reassigned (preferred over var)
+
+var email = "abc@gmail.com"       // can be reassigned (not preferred due to scope issue ) noitce we dont really need to use ;
+
+address = "goa"             // can assign variable without keyword
+
+let money;
+user = "avcii";
+
+console.table([accountId, user, email, address,money ]);
---
+++ 2_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";       // treat all JS code as newer version
+
+// alert(3 + 3)     // we are using nodejs, not browser
+
+let a = 23;
+let Name = "kapil";
+
+
+// number => 2 to power 53
+// bigint  => if wanna store large number
+// string => ""
+// boolena => true / false;
+// null => standalone value (it mean nothing not empty)
+// undefined => no value
+// symbol => for unqiueness
+// object =>  user defined
+
+console.log(typeof Name);
+
+console.log(typeof null);         // outout object
+
+console.log(typeof undefined);   // outputs undefined
