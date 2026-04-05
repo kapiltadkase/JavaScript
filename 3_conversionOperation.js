c6437228919add9ec01295425c629571ee2dfab4
kapiltadkase <kapiltadkase770@gmail.com>
2026-04-05 22:50:51 +0530

    Operations

--- 3_conversionOperation.js
+++ 3_conversionOperation.js
@@ -1,54 +1,88 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
-// or we can write it as method
-console.log(typeof(score));
+// console.log(typeof score);
+//  or we can write it as method
+// console.log(typeof(score));
 
-//Converting string to number
+//------------------------------------------------
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => Nan (not a number) type is number
 // true => 1 ; false => 0
 
-let a = 1
-let booleanA = Boolean(a);
+// let a = 1
+// let booleanA = Boolean(a);
 
-console.log(a);
-console.log(typeof a);
+// console.log(a);
+// console.log(typeof a);
 
-let str = "";
-let booleanStr = Boolean(str);
-console.log(booleanStr);
-console.log(typeof booleanStr);
+// let str = "";
+// let booleanStr = Boolean(str);
+// console.log(booleanStr);
+// console.log(typeof booleanStr);
 
 // 1 => true ; 0 => false
 // "" => false ; "lsafnf" => true
 
-let num = 33;
-let stringNum = String(33);
-console.log(stringNum);
-console.log(typeof stringNum);
+// let num = 33;
+// let stringNum = String(33);
+// console.log(stringNum);
+// console.log(typeof stringNum);
 
 // "33" => 33
 
 
-let temp = null;
-let booleanTemp = Boolean(undefined);
+// let temp = null;
+// let booleanTemp = Boolean(undefined);
 
-console.log(booleanTemp);
-console.log(typeof  booleanTemp);
+// console.log(booleanTemp);
+// console.log(typeof  booleanTemp);
 
 // undefined => false;
 // null => false;
 
-let flag = null;
-let stringFlag = String(flag);
-console.log(stringFlag);
-console.log(typeof stringFlag);
+//--------------------------------------------------
+
+// let flag = null;
+// let stringFlag = String(flag);
+// console.log(stringFlag);
+// console.log(typeof stringFlag);
 
 // null => "null"
+
+
+//======================= OPERATIONS===============================
+
+// let str1 = "hi";
+// let str2 = " kapil";
+// let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("2" + 4);
+// console.log(4 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+//--------------------------------------------
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1 ,num2, num3;
+
+// num1 = num2 = num3 = 2 + 2;
+
+//-------------------------------------------
+
+let cnt = 100;
+cnt++;
+console.log(cnt);
+
+++cnt;
+console.log(cnt);
